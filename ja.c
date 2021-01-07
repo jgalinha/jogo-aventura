8fc10d786316ba3fbf7389a6eb88f63741a169ff
jgalinha <j.b.galinha@gmail.com>
2021-01-07 13:08:14 +0000

    Update map and player functions in class

--- ja.c
+++ ja.c
@@ -4,9 +4,12 @@
 /* Email: jbgalinha@gmai.com *************************************************/
 /*****************************************************************************/
 
-#include "stdio.h"
+#include <stdio.h>
+#include <string.h>
 
 #define MAX_NAME 50 /* Maximum name length */
+#define MAX_TEXT_ROOM_DESCRIPTION 500 /* Maximum lenght for the room description */
+#define MAX_ROOMS 100 /* Maximum rooms in each map */
 #define INITIAL_ENERGY_PLAYER 100 /* Default start player energy */
 #define INITIAL_PLAYER_LOCATION 0 /* Defaule start player location */
 #define NONE -1 /* Constant do start empty things */
@@ -14,25 +17,52 @@
 /* Player Structure */
 struct Player {
     char name[MAX_NAME];
-    int energy;
-    int location;
-    int object;
-    int treasure;
+    short int energy;
+    short int location;
+    short int object;
+    short int treasure;
 };
 
+/* Room Structure */
+struct Room {
+    short int north;
+    short int south;
+    short int east;
+    short int west;
+    short int up;
+    short int down;
+    short int object;
+    short int treasure;
+    char description[MAX_TEXT_ROOM_DESCRIPTION];
+};
+
+/* Player Functions **********************************************************/
 void PlayerInit(struct Player *pPlayer); 
-void PlayerStats(struct Player *pPlayer);
+void PlayerStats(struct Player player);
+/* Map Functions *************************************************************/
+void RoomInit(struct Room *pRoom, short int north, short int south,
+              short int east, short int west, short int up, short int down,
+              short int object, short int treasure,
+              char description[MAX_TEXT_ROOM_DESCRIPTION]);
+/*****************************************************************************/
 
 int main() {
 
     struct Player player;
+    struct Room map[MAX_ROOMS];
+    short int nRoomMap;
 
     /* Player Initializations */
     PlayerInit(&player);
-    PlayerStats(&player);
+    PlayerStats(player);
     
     return 0;
 }
+
+/*****************************************************************************/
+/*                              Player Functions                             */
+/*****************************************************************************/
+
 
 void PlayerInit(struct Player *pPlayer) {
     printf("Qual o seu nome aventureiro? \n -> ");
@@ -44,7 +74,36 @@ void PlayerInit(struct Player *pPlayer) {
     printf("Boa Sorte %s! Vai precisar...\n", (*pPlayer).name);
 }
 
-void PlayerStats(struct Player *pPlayer) {
-    printf("*** Player %s Stats ***", pPlayer -> name);
+void PlayerStats(struct Player player) {
+    printf("*** Player %s Stats ***", player.name);
+}
+
+/*****************************************************************************/
+/*                               Map Functions                               */
+/*****************************************************************************/
+
+void InitDefaultMap() {}
+
+void MapInit(struct Room *pMap[]){
+
+    /* Devolver o número de sala em cada mapa */
+}
+
+/* TODO Alterar a funçao para ser passado tipo um vector ou mesmo um Room em vez das várias variáveis? */
+void RoomInit(struct Room *pRoom, short int north, short int south,
+              short int east, short int west, short int up, short int down,
+              short int object, short int treasure,
+              char description[MAX_TEXT_ROOM_DESCRIPTION]){
+
+    pRoom->north = north;
+    pRoom->south = south;
+    pRoom->east = east;
+    pRoom->west = west;
+    pRoom->up = up;
+    pRoom->down = down;
+    pRoom->object = object;
+    pRoom->treasure = treasure;
+    strcpy(pRoom->description, description);
+    
 }
 
