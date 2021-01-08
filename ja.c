4e05809675f66a24ef0c1f17e7a66b3012f0227b
jgalinha <j.b.galinha@gmail.com>
2021-01-08 00:15:55 +0000

    Add functions RoomInit and InitDefaultMap

--- ja.c
+++ ja.c
@@ -40,6 +40,7 @@ struct Room {
 void PlayerInit(struct Player *pPlayer); 
 void PlayerStats(struct Player player);
 /* Map Functions *************************************************************/
+short int InitDefaultMap(struct Room *pMap); 
 void RoomInit(struct Room *pRoom, short int north, short int south,
               short int east, short int west, short int up, short int down,
               short int object, short int treasure,
@@ -54,7 +55,11 @@ int main() {
 
     /* Player Initializations */
     PlayerInit(&player);
-    PlayerStats(player);
+    /* Object Initialization */
+    /* Map Initialization */
+    nRoomMap = InitDefaultMap(map);
+    /* Monster Initialization */
+    
     
     return 0;
 }
@@ -63,7 +68,7 @@ int main() {
 /*                              Player Functions                             */
 /*****************************************************************************/
 
-
+/* Function to init the player */
 void PlayerInit(struct Player *pPlayer) {
     printf("Qual o seu nome aventureiro? \n -> ");
     scanf("%s", (*pPlayer).name);
@@ -71,9 +76,10 @@ void PlayerInit(struct Player *pPlayer) {
     (*pPlayer).location = INITIAL_PLAYER_LOCATION;
     (*pPlayer).object = NONE; 
     (*pPlayer).treasure = NONE;
-    printf("Boa Sorte %s! Vai precisar...\n", (*pPlayer).name);
+    printf("\nBoa Sorte %s! Vai precisar...\n", (*pPlayer).name);
 }
 
+/* Function to show the player stats */
 void PlayerStats(struct Player player) {
     printf("*** Player %s Stats ***", player.name);
 }
@@ -82,14 +88,29 @@ void PlayerStats(struct Player player) {
 /*                               Map Functions                               */
 /*****************************************************************************/
 
-void InitDefaultMap() {}
+/* Function to initialize one default map ************************************/
+short int InitDefaultMap(struct Room *pMap) {
+    /* TODO Create the default map layout */
+    RoomInit(&pMap[0], 1, 1, 1, 1, NONE, NONE, 1, 1, "Teste1");
+    RoomInit(&pMap[1], 1, 1, 1, 1, NONE, NONE, 1, 1, "Teste3");
+    RoomInit(&pMap[2], 1, 1, 1, 1, NONE, NONE, 1, 1, "Teste3");
+    RoomInit(&pMap[3], 1, 1, 1, 1, NONE, NONE, 1, 1, "Teste3");
+    RoomInit(&pMap[4], 1, 1, 1, 1, NONE, NONE, 1, 1, "Teste3");
+    RoomInit(&pMap[5], 1, 1, 1, 1, NONE, NONE, 1, 1, "Teste3");
+    RoomInit(&pMap[6], 1, 1, 1, 1, NONE, NONE, 1, 1, "Teste3");
+    RoomInit(&pMap[7], 1, 1, 1, 1, NONE, NONE, 1, 1, "Teste3");
+    RoomInit(&pMap[8], 1, 1, 1, 1, NONE, NONE, 1, 1, "Teste3");
+    RoomInit(&pMap[9], 1, 1, 1, 1, NONE, NONE, 1, 1, "Teste9");
+
+    return 10;
+}
 
 void MapInit(struct Room *pMap[]){
 
     /* Devolver o número de sala em cada mapa */
 }
 
-/* TODO Alterar a funçao para ser passado tipo um vector ou mesmo um Room em vez das várias variáveis? */
+/* Function to init the map rooms */
 void RoomInit(struct Room *pRoom, short int north, short int south,
               short int east, short int west, short int up, short int down,
               short int object, short int treasure,
