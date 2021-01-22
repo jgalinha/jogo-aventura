56a1f2ec6714f880ec35848b50c9c3393a65b110
jgalinha <j.b.galinha@gmail.com>
2021-01-22 01:31:14 +0000

    Add moster init func

--- ja.c
+++ ja.c
@@ -6,13 +6,19 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define MAX_NAME 50 /* Maximum name length */ 
+#define MAX_OBJECT_NAME 10 /* Maximum object name lenght */
 #define MAX_TEXT_ROOM_DESCRIPTION 500 /* Maximum lenght for the room description */
 #define MAX_ROOMS 100 /* Maximum rooms in each map */
 #define INITIAL_ENERGY_PLAYER 100 /* Default start player energy */
 #define INITIAL_PLAYER_LOCATION 0 /* Defaule start player location */
 #define NONE -1 /* Constant do start empty things */
+
+
+short int nRoomMap; /* Number of rooms in the map */
 
 /* Player Structure */
 struct Player {
@@ -36,6 +42,18 @@ struct Room {
     char description[MAX_TEXT_ROOM_DESCRIPTION];
 };
 
+/* Object Structure */
+struct Object {
+    char name[MAX_OBJECT_NAME];
+    short int power;
+};
+
+/* Monster Structure */
+struct Monster {
+    short int energy;
+    short int location;
+};
+
 /* Player Functions **********************************************************/
 void PlayerInit(struct Player *pPlayer); 
 void PlayerStats(struct Player player);
@@ -45,21 +63,31 @@ void RoomInit(struct Room *pRoom, short int north, short int south,
               short int east, short int west, short int up, short int down,
               short int object, short int treasure,
               char description[MAX_TEXT_ROOM_DESCRIPTION]);
+/* Object Functions **********************************************************/
+short int ObjectsInit(struct Object *pObjects[]); 
+void ObjectInit(struct Object *pObject, char name[MAX_OBJECT_NAME],
+                short int power); 
+short int DefaultObjectsInit(struct Object *pObject); 
+/* Monster Functions *********************************************************/
+void MonsterInit (struct Monster *pMonster, short int energy,
+                  short int location); 
 /*****************************************************************************/
 
 int main() {
 
     struct Player player;
     struct Room map[MAX_ROOMS];
-    short int nRoomMap;
-
+    struct Object objects[MAX_ROOMS];
+    struct Monster monster;
+    short int nObjects;
     /* Player Initializations */
     PlayerInit(&player);
     /* Object Initialization */
+    DefaultObjectsInit(objects);
     /* Map Initialization */
     nRoomMap = InitDefaultMap(map);
     /* Monster Initialization */
-    
+    MonsterInit(&monster, 100, -1);
     
     return 0;
 }
@@ -69,7 +97,7 @@ int main() {
 /*****************************************************************************/
 
 /* Function to init the player */
-void PlayerInit(struct Player *pPlayer) {
+void PlayerInit(struct Player *pPlayer) { // (ref:PlayerInit)
     printf("Qual o seu nome aventureiro? \n -> ");
     scanf("%s", (*pPlayer).name);
     (*pPlayer).energy = INITIAL_ENERGY_PLAYER;
@@ -126,4 +154,46 @@ void RoomInit(struct Room *pRoom, short int north, short int south,
     pRoom->treasure = treasure;
     strcpy(pRoom->description, description);
     
+}
+
+/* Function to initialize de objects vector */
+short int ObjectsInit(struct Object *pObjects[]) {
+    return 0;
+}
+
+/* Function to initialize an object */
+void ObjectInit(struct Object *pObject, char name[MAX_OBJECT_NAME],
+                short int power) {
+    strcpy(pObject->name, name);    
+    pObject->power = power;
+}
+
+/* Function to initialize the default objects vector */
+short int DefaultObjectsInit(struct Object *pObject) {
+    ObjectInit(&pObject[0], "faca", 5);
+    ObjectInit(&pObject[1], "espada", 20);
+    ObjectInit(&pObject[2], "escudo", -10);
+    ObjectInit(&pObject[3], "sopa", -3);
+    ObjectInit(&pObject[4], "alabarda", 35);
+    ObjectInit(&pObject[5], "machado", 45);
+    ObjectInit(&pObject[5], "besta", 20);
+    ObjectInit(&pObject[6], "poção mágica", -80);
+
+    return 7;
+}
+
+void MonsterInit (struct Monster *pMonster, short int energy,
+                  short int location) {
+    
+    time_t t;
+    
+    pMonster->energy = energy;
+    if (location == -1 || location > nRoomMap) {
+        do {
+            /* Intializes random number generator */
+            srand((unsigned) time(&t));
+            location = rand() % nRoomMap;
+        } while (location <= 1);
+    } 
+    pMonster->location = location;
 }
